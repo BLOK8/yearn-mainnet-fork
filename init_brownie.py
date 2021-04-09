53f6058710441984272965d0fe09273459525e9a
navneetgulati <navneet.gulati@block8.io>
2021-04-09 12:50:22 +0530

    Make it compatible with USDC

--- init_brownie.py
+++ init_brownie.py
@@ -7,6 +7,6 @@ def main():
   # It's pretty handy for caching docker layers and installing all necessary compilers at build time.
   # TODO extend this list with all relevant solidity versions
   print("installing compiler dependencies for brownie")
-  Contract.from_explorer("0xD533a949740bb3306d119CC777fa900bA034cd52") #crvToken
-  Contract.from_explorer("0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e") #yfiToken
-  Contract.from_explorer("0x6b175474e89094c44da98b954eedeac495271d0f") #daiToken
+  # Contract.from_explorer("0xD533a949740bb3306d119CC777fa900bA034cd52") #crvToken
+  # Contract.from_explorer("0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e") #yfiToken
+  Contract.from_explorer("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48") #usdcToken
--- supply_tokens.py
+++ supply_tokens.py
@@ -8,15 +8,15 @@ def main():
         "0x5307DB0c8Fb2Dc616B94f3b2f9fe7a99920e5e52"
     ]
 
-    crvToken = Contract.from_explorer("0xD533a949740bb3306d119CC777fa900bA034cd52")
-    yfiToken = Contract.from_explorer("0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e")
-    daiToken = Contract.from_explorer("0x6b175474e89094c44da98b954eedeac495271d0f")
+    # crvToken = Contract.from_explorer("0xD533a949740bb3306d119CC777fa900bA034cd52")
+    # yfiToken = Contract.from_explorer("0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e")
+    usdcToken = Contract.from_explorer("0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48")
 
-    crvWhale = accounts.at("0x4ce799e6ed8d64536b67dd428565d52a531b3640", force=True)
-    yfiWhale = accounts.at("0xfeb4acf3df3cdea7399794d0869ef76a6efaff52", force=True)
-    daiWhale = accounts.at("0xc3d03e4f041fd4cd388c549ee2a29a9e5075882f", force=True)
+    # crvWhale = accounts.at("0x4ce799e6ed8d64536b67dd428565d52a531b3640", force=True)
+    # yfiWhale = accounts.at("0xfeb4acf3df3cdea7399794d0869ef76a6efaff52", force=True)
+    usdcWhale = accounts.at("0x397ff1542f962076d0bfe58ea045ffa2d347aca0", force=True)
 
     for account in testAccounts:
-        crvToken.transfer(account, "1000 ether", {'from': crvWhale})
-        yfiToken.transfer(account, "250 ether", {'from': yfiWhale})
-        daiToken.transfer(account, "1000000 ether", {'from': daiWhale})
+        # crvToken.transfer(account, "100", {'from': crvWhale})
+        # yfiToken.transfer(account, "20", {'from': yfiWhale})
+        usdcToken.transfer(account, "1000000000", {'from': usdcWhale})
